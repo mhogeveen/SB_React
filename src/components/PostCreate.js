e2655464daae03dade06f8f57f7dde3bdfe9bfaa
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-09-02 15:55:05 +0200

    Fix compatability issues for Safari

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const PostCreate = () => {
-   return <div className='post-create'></div>
+   return (
+      <div className='post-create'>
+         <form>
+            <label></label>
+            <input />
+            <label></label>
+            <input />
+            <label></label>
+            <textarea />
+         </form>
+      </div>
+   )
 }
 
 export default PostCreate
