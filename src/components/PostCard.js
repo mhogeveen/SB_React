6c1325c42c3688db8aeb69fc8603b072f3a0a251
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-09-02 14:49:33 +0200

    Refactor PostCard and truncate content length

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const PostCard = ({ post }) => {
+   const { created_at, category, title, content } = post
+
    return (
       <article className='post-card'>
          <div
@@ -8,15 +10,15 @@ const PostCard = ({ post }) => {
             style={{ background: `url('https://picsum.photos/200/300?random') rgba(0,0,0,0.4)` }}
          >
             <span className='header__meta meta--date'>
-               {new Date(post.created_at).toLocaleDateString()}
+               {new Date(created_at).toLocaleDateString()}
             </span>
-            <span className='header__meta meta--category'>
-               {post.category ? post.category.name : ''}
-            </span>
+            <span className='header__meta meta--category'>{category ? category.name : ''}</span>
          </div>
          <div className='post-card__content'>
-            <span className='content__title'>{post.title}</span>
-            <p className='content__summary'>{post.content}</p>
+            <span className='content__title'>{title}</span>
+            <p className='content__summary'>
+               {content.length > 180 ? content.slice(0, 180) + '...' : content}
+            </p>
          </div>
       </article>
    )
