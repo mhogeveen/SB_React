8e86e7a998019bb06cc7f149d0d0db90b6879a3e
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-09-02 16:58:37 +0200

    Add first pass for post submit form

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -3,14 +3,24 @@ import React from 'react'
 const PostCreate = () => {
    return (
       <div className='post-create'>
-         <form>
-            <label></label>
-            <input />
-            <label></label>
-            <input />
-            <label></label>
-            <textarea />
-         </form>
+         <div className='form__item item--title'>
+            <label>Berichtnaam</label>
+            <input type='text' name='berichtnaam' placeholder='Geen titel' />
+         </div>
+         <div className='form__item item--category'>
+            <label>Categorie</label>
+            <select type='select' name='categorie' defaultValue='Geen categorie'>
+               <option value=''>Geen categorie</option>
+               <option>Categorie 1</option>
+               <option>Categorie 2</option>
+               <option>Categorie 3</option>
+            </select>
+         </div>
+         <div className='form__item item--message'>
+            <label>Bericht</label>
+            <textarea name='bericht' />
+         </div>
+         <button className='btn'>Bericht aanmaken</button>
       </div>
    )
 }
